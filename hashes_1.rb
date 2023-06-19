ad4885e16e727b58e26070bd0e607a86e275f6de
Antoine Aube <aaube3@gmail.com>
2023-06-19 13:18:24 -0600

    add solutions to hashes_1 and hashes_2 exercises

--- hashes_1.rb
+++ hashes_1.rb
@@ -1,70 +1,87 @@
 
-Activity:
+# # Activity:
 
-# In the below exercises, write code that achieves
-# the desired result. To check your work, run this
-# file by entering the following command in your Terminal: 
-# `ruby hashes_1.rb`
+# # In the below exercises, write code that achieves
+# # the desired result. To check your work, run this
+# # file by entering the following command in your Terminal: 
+# # `ruby hashes_1.rb`
 
-foods = {"apples": 23, "grapes": 507, "eggs": 48}
-p zoo
-
-# Write code that prints all of the 'keys' of the foods variable 
-# you created above:
-# YOUR CODE HERE
-
-# Write code that prints all of the 'values' of the foods variable 
-# you created above:
-# YOUR CODE HERE
-
-# Write code that prints the value of the second food of the foods variable 
-# you created above:
-# YOUR CODE HERE
-
-# Write code that adds a food to the foods hash. 
-# Then, print the updated hash:
-# YOUR CODE HERE
+# foods = {
+#   apples: 23,
+#   grapes: 507,
+#   eggs: 48
+# }
 
 
-#-------------------
-# Part 2: Email
-#-------------------
+# # Write code that prints all of the 'keys' of the foods variable 
+# # you created above:
+# # YOUR CODE HERE
+# p foods.keys
+
+# # Write code that prints all of the 'values' of the foods variable 
+# # you created above:
+# # YOUR CODE HERE
+# p foods.values
+
+# # Write code that prints the value of the second food of the foods variable 
+# # you created above:
+# # YOUR CODE HERE
+# p foods.values[1]
+
+# # Write code that adds a food to the foods hash. 
+# # Then, print the updated hash:
+# # YOUR CODE HERE
+# foods[:carrots] = 12
+# p foods.keys
+
+# #-------------------
+# # Part 2: Email
+# #-------------------
 
 
-# Think about all the pieces of information associated with one single email in your inbox.
-# It has a sender, a subject, ...
+# # Think about all the pieces of information associated with one single email in your inbox.
+# # It has a sender, a subject, ...
 
-# Declare a variable that stores hash. Each key should be an attribute of an email and each
-# value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
+# # Declare a variable that stores hash. Each key should be an attribute of an email and each
+# # value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
 
-# Write code that prints your email hash to the terminal.
+# email1 = {
+#   recipient: "Boss@job.com",
+#   sender: "me@me.com",
+#   time_stamp: "12:00am",
+#   subject: "Resignation",
+#   body: "I quit!"
+# }
+
+# # Write code that prints your email hash to the terminal.
+# p email1
+
+# # Write code that prints all of the 'keys' of the email hash 
+# # you created above:
+# # YOUR CODE HERE
+# p email1.keys
+
+# # Write code that prints all of the 'values' of the email hash 
+# # you created above:
+# # YOUR CODE HERE
+# p email1.values
+
+# #-------------------
+# # Part 3: Many Emails - CHALLENGE!
+# #-------------------
+
+# # LONG EXAMPLE:
+# # Now that we've learned about Hashes AND Arrays, we can combine them.
+
+# # Check out the following example of an array of Instagram posts:
 
 
-# Write code that prints all of the 'keys' of the email hash 
-# you created above:
-# YOUR CODE HERE
-
-# Write code that prints all of the 'values' of the email hash 
-# you created above:
-# YOUR CODE HERE
+# posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"]
 
 
-#-------------------
-# Part 3: Many Emails - CHALLENGE!
-#-------------------
-
-# LONG EXAMPLE:
-# Now that we've learned about Hashes AND Arrays, we can combine them.
-
-# Check out the following example of an array of Instagram posts:
-
-
-posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"]
-
-
-# An Array of Hashes is probably more realistic. Hashes are a data type that's great
-# for storing more complex data. Below, we can store multiple key-value pairs
-# that relate to each individual Instagram post:
+# # An Array of Hashes is probably more realistic. Hashes are a data type that's great
+# # for storing more complex data. Below, we can store multiple key-value pairs
+# # that relate to each individual Instagram post:
 
 
 posts = [
@@ -97,3 +114,24 @@ p posts[0]
 # YOU DO: Create an array of at least 3 EMAIL Hashes, using the same 
 # key-value pairs you used in your email Hash above.
 # Then, print the email Array to the Terminal.
+
+emails = [
+  {
+    email_number: "1",
+    recipient: "Boss@job.com",
+    sender: "me@me.com",
+    time_stamp: "12:00am",
+    subject: "Resignation",
+    body: "I quit!"
+  },
+
+  {email_number: "2",
+    recipient: "Boss@job.com",
+    sender: "me@me.com",
+    time_stamp: "6:00am",
+    subject: "Take me back!",
+    body: "I'm so sorry, my cat typed that email and I don't actually want to quit"
+  }
+]
+
+p emails
